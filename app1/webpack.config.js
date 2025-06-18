2bc45bc101e1c4615cae183182f430b94e40c876
higherorder <higherorder>
2025-06-18 20:22:48 +0800

    run time remote entry

--- app1/webpack.config.js
+++ app1/webpack.config.js
@@ -4,6 +4,37 @@ const CleanWebpackPlugin = require("clean-webpack-plugin").CleanWebpackPlugin;
 const path = require('path');
 const { name } = require('./package.json');
 
+// 动态获取 remoteEntry.js 的 URL，解决缓存问题
+const getRemoteEntry = (remoteName, publicPath) => {
+    return `promise new Promise(resolve => {
+                const now = new Date().getTime();
+                const remoteUrl =  "${publicPath}remoteEntry.js?t=" + now; // 比如从全局变量中获取
+                const scriptId = "remote-${remoteName}"
+                let script = document.getElementById(scriptId);
+                if (!script) {
+                    script = document.createElement('script');
+                    script.setAttribute('id', scriptId);
+                }
+                script.src = remoteUrl;
+                script.onload = () => {
+                    resolve({
+                        get: (request) => window.${remoteName}.get(request),
+                        init: (arg) => {
+                            try {
+                                return window.${remoteName}.init(arg);
+                            } catch (e) {
+                                console.log('remote container already initialized');
+                            }
+                        }
+                    });
+                };
+                script.onerror = () => {
+                    reject(new Error('Failed to load ${remoteName} module'));
+                };
+                document.head.appendChild(script);
+            })`;
+}
+
 module.exports = {
     entry: {
         app: "./src/index"
@@ -52,13 +83,14 @@ module.exports = {
         new ModuleFederationPlugin({
             name: "app1",
             remotes: {
-                'app2plus': "app2plus@http://localhost:3002/remoteEntry.js",
+                // 'app2plus': "app2plus@http://localhost:3002/remoteEntry.js",
+                app2plus: getRemoteEntry("app2plus", "http://localhost:3002/")
             },
             shared: { react: { eager: true, singleton: true }, "react-dom": { eager: true, singleton: true } },
         }),
         new HtmlWebpackPlugin({
             template: "./public/index.html",
-            chunks: ['app2plus','app'],
+            chunks: ['app2plus', 'app'],
             chunksSortMode: "manual"
         }),
     ],
